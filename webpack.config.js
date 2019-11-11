e401cd8f81d1864fa00f7d900ae6d135c242f91d
jello <2511841570@qq.com>
2019-11-11 18:56:56 +0800

    :sparkles: vm适配移动端

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 module.exports = {
-  mode: 'production',
-  entry: './src/App.jsx',
+  mode: "production",
+  entry: "./src/App.jsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[chunkhash].js"
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"]
       },
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({hash:true}), new CleanWebpackPlugin({template: './dist'})]
-}
+  plugins: [
+    new HtmlWebpackPlugin({ hash: true }),
+    new CleanWebpackPlugin({ template: "./dist" })
+  ]
+};
