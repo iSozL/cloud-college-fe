f8afae0ccdd1ff3f94d7c1c47c519d696245b571
jello <2511841570@qq.com>
2019-11-12 20:32:40 +0800

    :sparkles: 增加loading

--- src/index/Index.jsx
+++ src/index/Index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 import Button from "../components/button/Button.jsx";
+import Register from "../register/Register.jsx";
 function Index() {
   return (
     <div className="index-container">
@@ -8,8 +10,13 @@ function Index() {
         src="https://have-more-boiled-water.oss-cn-shanghai.aliyuncs.com/%E5%BC%80%E5%A7%8B.png"
         className="index-logo"
       />
-      <Button name="登录" style={{ margin: "200px 0 0 0" }} />
-      <Button name="注册" style={{ margin: "50px 0 0 0" }} />
+
+      <Link to="/login" style={{ textDecoration: "none" }}>
+        <Button name="登录" style={{ margin: "200px 0 0 0" }} />
+      </Link>
+      <Link to="" style={{ textDecoration: "none" }}>
+        <Button name="注册" style={{ margin: "50px 0 0 0" }} />
+      </Link>
       <div
         style={{
           fontSize: "40px",
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import Index from "./index/Index.jsx";
-import Register from "./register/Register.jsx";
+const Index = React.lazy(() => import("./index/Index.jsx"));
+const Login = React.lazy(() => import("./register/Register.jsx"));
 class Routes extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Index} />
-          <Route path="/register" component={Register} />
-        </Switch>
-      </BrowserRouter>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Index} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </React.Suspense>
     );
   }
 }
