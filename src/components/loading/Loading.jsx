1603e8b009a917ab9c373b8dba5bfc3f3e05a966
jello <2511841570@qq.com>
2019-11-12 21:42:51 +0800

    :lipstick: 增加loading样式

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loading.css";
+function Loading() {
+  return (
+    <div className="img">
+      <img
+        src="https://have-more-boiled-water.oss-cn-shanghai.aliyuncs.com/loading.gif"
+      ></img>
+    </div>
+  );
+}
+export default Loading;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 const Index = React.lazy(() => import("./index/Index.jsx"));
 const Login = React.lazy(() => import("./register/Register.jsx"));
-const Register = React.lazy(() => import("./login/Login.jsx"))
-const Forget = React.lazy(() => import("./forget/Forget.jsx"))
+const Register = React.lazy(() => import("./login/Login.jsx"));
+const Forget = React.lazy(() => import("./forget/Forget.jsx"));
+import Loading from "./components/loading/Loading.jsx";
 class Routes extends React.Component {
   render() {
     return (
-      <React.Suspense fallback={<div>Loading...</div>}>
+      <React.Suspense fallback={Loading}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Index} />
