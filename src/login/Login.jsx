eda2750e2c5372f00cad4f5aeb618e66003b4cae
jello <2511841570@qq.com>
2019-11-13 17:35:02 +0800

    :speech_balloon: 更新提示信息

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -4,61 +4,71 @@ import InputBox from "../components/inputBox/InputBox.jsx";
 import Button from "../components/button/Button.jsx";
 import TextBox from "../components/textBox/TextBox.jsx";
 import { Link } from "react-router-dom";
-import Api from "../utils/apiUtils.jsx"
+import Api from "../utils/apiUtils.jsx";
+import "antd/lib/message/style/index.css";
+import message from "antd/lib/message/index.js";
 class Login extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       username: "",
       password: "",
       email: "",
       confirmPassword: ""
-    }
-    this.setUsername = this.setUsername.bind(this)
-    this.setPassword = this.setPassword.bind(this)
-    this.setEmail = this.setEmail.bind(this)
-    this.confirmPassword = this.confirmPassword.bind(this)
-    this.register = this.register.bind(this)
+    };
+    this.setUsername = this.setUsername.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.confirmPassword = this.confirmPassword.bind(this);
+    this.register = this.register.bind(this);
   }
   setUsername(e) {
     this.setState({
       username: e.target.value
-    })
+    });
   }
   setEmail(e) {
     this.setState({
       email: e.target.value
-    })
+    });
   }
   setPassword(e) {
     this.setState({
       password: e.target.value
-    })
+    });
   }
   confirmPassword(e) {
     this.setState({
       confirmPassword: e.target.value
-    })
+    });
   }
   register() {
-    const registerMessage = {
-      username: this.state.username,
-      password: this.state.password,
-      email: this.state.email
-    }
-    if(this.state.password == this.state.confirmPassword) {
-      Api.fetchData('/register', 'post', registerMessage).then(
-        res => {
-          if(res.status == 200) {
-            console.log(res)
+    if(this.state.password && this.state.email && this.state.username) {
+      const registerMessage = {
+        username: this.state.username,
+        password: this.state.password,
+        email: this.state.email
+      };
+      if (this.state.password == this.state.confirmPassword) {
+        Api.fetchData("/register", "post", registerMessage).then(
+          res => {
+            if (res.status == 200) {
+              if(res.data.status == 1) {
+                message.success(res.data.message)
+              } else {
+                message.error(res.data.message)
+              }
+            }
+          },
+          error => {
+            message.error(error);
           }
-        },
-        error => {
-          console.log(error)
-        }
-      )
+        );
+      } else {
+        message.error("输入的密码不一致");
+      }
     } else {
-      console.log("输入的密码不一致")
+      message.info("请输入完整")
     }
   }
   render() {
@@ -79,10 +89,22 @@ class Login extends React.Component {
           >
             <InputBox placeholder="用户名" onChange={this.setUsername} />
             <InputBox placeholder="邮箱" onChange={this.setEmail} />
-            <InputBox placeholder="密码" type="password" onChange={this.setPassword} />
-            <InputBox placeholder="确认密码" type="password" onChange={this.confirmPassword} />
+            <InputBox
+              placeholder="密码"
+              type="password"
+              onChange={this.setPassword}
+            />
+            <InputBox
+              placeholder="确认密码"
+              type="password"
+              onChange={this.confirmPassword}
+            />
           </div>
-          <Button name="注册" style={{ margin: "60px 0 0 0" }} onClick={this.register} />
+          <Button
+            name="注册"
+            style={{ margin: "60px 0 0 0" }}
+            onClick={this.register}
+          />
           <Link to="/login" style={{ textDecoration: "none" }}>
             <div
               style={{
--- src/register/Register.jsx
+++ src/register/Register.jsx
@@ -4,49 +4,53 @@ import InputBox from "../components/inputBox/InputBox.jsx";
 import Button from "../components/button/Button.jsx";
 import TextBox from "../components/textBox/TextBox.jsx";
 import { Link } from "react-router-dom";
-import Api from '../utils/apiUtils.jsx'
-import 'antd/lib/message/style/index.css';
-import message from 'antd/lib/message/index.js';
+import Api from "../utils/apiUtils.jsx";
+import "antd/lib/message/style/index.css";
+import message from "antd/lib/message/index.js";
 class Rigster extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       username: "",
       password: ""
-    }
-    this.getUsername = this.getUsername.bind(this)
-    this.getPassword = this.getPassword.bind(this)
-    this.login = this.login.bind(this)
+    };
+    this.getUsername = this.getUsername.bind(this);
+    this.getPassword = this.getPassword.bind(this);
+    this.login = this.login.bind(this);
   }
   getUsername(e) {
     this.setState({
       username: e.target.value
-    })
+    });
   }
   getPassword(e) {
     this.setState({
       password: e.target.value
-    })
+    });
   }
   login() {
-    const user = {
-      username: this.state.username,
-      password: this.state.password
-    }
-    Api.fetchData('/login', 'post', user).then(
-      res => {
-        if(res.status == 200) {
-          if(res.data.status == 1) {
-            message.success(res.data.message)
-          } else {
-            message.error(res.data.message)
-          }         
+    if (this.state.username && this.state.password) {
+      const user = {
+        username: this.state.username,
+        password: this.state.password
+      };
+      Api.fetchData("/login", "post", user).then(
+        res => {
+          if (res.status == 200) {
+            if (res.data.status == 1) {
+              message.success(res.data.message);
+            } else {
+              message.error(res.data.message);
+            }
+          }
+        },
+        error => {
+          message.error(error);
         }
-      },
-      error => {
-        console.log(error)
-      }
-    )
+      );
+    } else {
+      message.info("请输入完整");
+    }
   }
   render() {
     return (
@@ -65,7 +69,11 @@ class Rigster extends React.Component {
             }}
           >
             <InputBox placeholder="用户名" onChange={this.getUsername} />
-            <InputBox placeholder="密码" type="password" onChange={this.getPassword} />
+            <InputBox
+              placeholder="密码"
+              type="password"
+              onChange={this.getPassword}
+            />
             <div
               style={{
                 alignSelf: "flex-end",
@@ -81,7 +89,11 @@ class Rigster extends React.Component {
               </Link>
             </div>
           </div>
-          <Button name="登录" style={{ margin: "240px 0 100px 0" }} onClick={this.login} />
+          <Button
+            name="登录"
+            style={{ margin: "240px 0 100px 0" }}
+            onClick={this.login}
+          />
         </TextBox>
       </div>
     );
