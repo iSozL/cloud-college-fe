8078065d26435280413ac115675e1fe9c7d9e84a
jello <2511841570@qq.com>
2019-11-19 20:02:51 +0800

    :lipstick: 完善房间信息样式

--- src/roomMsg/RoomMsg.jsx
+++ src/roomMsg/RoomMsg.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Back from "../components/back/Back.jsx";
-import TextBox from "../components/textBox/TextBox.jsx"
-import "./roomMsg.css"
+import TextBox from "../components/textBox/TextBox.jsx";
+import Button from "../components/button/Button.jsx"
+import "./roomMsg.css";
 function RoomMsg() {
   return (
     <div className="users-container">
@@ -10,7 +11,28 @@ function RoomMsg() {
       </div>
       <div className="users-textbox-container">
         <TextBox>
-          <div>--</div>
+          <div className="room-msg-container">
+            <div className="room-title">—房间信息—</div>
+            <div className="room-body">
+              <div>寝室名：xxxx</div>
+              <div>寝室号：xxxx</div>
+              <div>
+                <div>室友(入驻天数)</div>
+                <div>1(1)</div>
+                <div>1(1)</div>
+                <div>1(1)</div>
+                <div>1(1)</div>
+              </div>
+              <div>房间状态：已满</div>
+              <div>房间型号：四人房</div>
+              <div>创建天数：100天</div>
+              <div>房间密匙：xxxxx</div>
+            </div>
+            <div className="room-button">
+              <Button name="退房" />
+              <Button name="更改" />
+            </div>
+          </div>
         </TextBox>
       </div>
     </div>
